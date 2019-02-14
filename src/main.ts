3b70862ac2c43a24fd56b1cd78d9c8ace5a5d66b
Dmitriy Melnichenko <tairezzzz@gmail.com>
2019-02-14 16:44:36 +0200

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+
+import * as dotenv from 'dotenv';
+
+import { AppModule } from './app.module';
+
+dotenv.load({path: '.env'});
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
