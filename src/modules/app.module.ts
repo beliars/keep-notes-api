6062c197099f500f5d6b467fc0e50246c0ce36ab
Dmitriy Melnichenko <tairezzzz@gmail.com>
2019-02-14 17:04:15 +0200

    feat (modules) add core & shared modules

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
+
 
 @Module({
   imports: [
+    CoreModule,
+    SharedModule,
     AuthModule,
   ],
 })
