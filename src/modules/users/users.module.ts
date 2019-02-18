3eab9cebd18df4dcca84658902881efa7d2d76f4
Dmitriy Melnichenko <tairezzzz@gmail.com>
2019-02-18 16:54:17 +0200

    feat (modules:users) add create user

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 
+import { SharedModule } from '../shared/shared.module';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
+  imports: [
+    SharedModule,
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { User } from '../core/interfaces/user.interface';
+import { SignupDto } from '../core/dto/signup.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  
+  constructor(
+    @InjectModel('User') private readonly userModel: Model<User>,
+  ) {
+  
+  }
+  
+  async create(user: SignupDto): Promise<User> {
+    const newUser: SignupDto = new SignupDto(user);
+    const createdUser = new this.userModel(newUser);
+    return await createdUser.save();
+  }
+  
+}
