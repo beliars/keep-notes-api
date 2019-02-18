37562757a439dc5e749018f92f07b4ff2816e7b7
Dmitriy Melnichenko <tairezzzz@gmail.com>
2019-02-18 16:53:35 +0200

    feat (modules:core) update mongoose options for deprication errors fix

--- src/modules/core/core.module.ts
+++ src/modules/core/core.module.ts
@@ -3,15 +3,20 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { SharedModule } from '../shared/shared.module';
 import { AuthService } from '../auth/auth.service';
+import { UsersService } from '../users/users.service';
 
 const SERVICES = [
   AuthService,
+  UsersService,
 ];
 
 @Global()
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest-rest'),
+    MongooseModule.forRoot('mongodb://localhost/nest-rest', {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+    }),
     SharedModule,
   ],
   providers: [
