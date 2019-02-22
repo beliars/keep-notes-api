28bb12e8315ececf9593b6ecf3c0bb2c32e3da64
Dmitriy Melnichenko <tairezzzz@gmail.com>
2019-02-22 13:11:14 +0200

    feat (shared) add token schema to imports

--- src/modules/shared/shared.module.ts
+++ src/modules/shared/shared.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
-import { UserSchema } from '../core/schemas/user.schema';
 import { ErrorsModule } from './errors/errors.module';
+
+import { UserSchema } from '../core/schemas/user.schema';
+import { TokenSchema } from '../core/schemas/token.schema';
 
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       {name: 'User', schema: UserSchema},
+      {name: 'Token', schema: TokenSchema},
     ]),
     ErrorsModule,
   ],
