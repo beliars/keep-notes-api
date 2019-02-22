441cb8d484ab05cabb94e6ec9cdd33081c880987
Dmitriy Melnichenko <tairezzzz@gmail.com>
2019-02-22 13:39:25 +0200

    feat (auth) add signup/signin

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, HttpStatus, Post, Req, Res } from '@nestjs/common';
 
 import { SignupDto } from '../core/dto/signup.dto';
+import { SigninDto } from '../core/dto/signin.dto';
 import { User } from '../core/interfaces/user.interface';
 import { Token } from '../core/interfaces/token.interface';
 import { UsersService } from '../users/users.service';
@@ -18,13 +19,18 @@ export class AuthController {
   @Post('signup')
   async signup(@Req() req: any, @Res() res: any, @Body() body: SignupDto,
   ) {
-    const user: User = await this.usersService.create(body);
-
-    //const auth: User = await this.authService.auth(body);
-    //const token: Token = await this.authService.createToken(auth._id);
-    //return res.status(HttpStatus.OK).send({token, user: auth});
-
-    return res.status(HttpStatus.OK).send(user);
+    await this.usersService.create(body);
+    const auth: User = await this.authService.auth(body);
+    const token: Token = await this.authService.createToken(auth._id);
+    return res.status(HttpStatus.OK).send({token, user: auth});
+  }
+  
+  @Post('signin')
+  async signin(@Req() req: any, @Res() res: any, @Body() body: SigninDto,
+  ) {
+    const auth: User = await this.authService.auth(body);
+    const token: Token = await this.authService.createToken(auth._id);
+    return res.status(HttpStatus.OK).send({token, user: auth});
   }
   
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,14 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt-nodejs';
+import * as jwt from 'jsonwebtoken';
+import * as moment from 'moment';
+
+import { SigninDto } from '../core/dto/signin.dto';
+import { User } from '../core/interfaces/user.interface';
+import { Token } from '../core/interfaces/token.interface';
 
 @Injectable()
 export class AuthService {
   
-  constructor() {
+  constructor(
+    @InjectModel('User') private readonly userModel: Model<User>,
+    @InjectModel('Token') private readonly tokenModel: Model<Token>,
+  ) {
   }
   
-  static retriveSafeUser(user) {
+  static retrieveSafeUser(user) {
     const {password, ...restUser} = user;
     return restUser;
+  }
+  
+  async auth(signinData: SigninDto): Promise<User> {
+    if (!signinData.email) {
+      throw new HttpException('Email is required', HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+    return this.userModel
+    .findOne({email: signinData.email.toLowerCase()})
+    .lean()
+    .then((user: any): Promise<any> => {
+      if (!user) {
+        throw new HttpException('Wrong email or password', HttpStatus.UNPROCESSABLE_ENTITY);
+      }
+      return new Promise((resolve, reject) => {
+        bcrypt.compare(signinData.password, user.password, (err, match) => {
+          if (err || !match) {
+            return reject({ message: 'Wrong email or password', status: 422 });
+          }
+          return resolve(AuthService.retrieveSafeUser(user));
+        });
+      });
+    });
+  }
+  
+  async createToken(userId: string): Promise<Token> {
+    return this.tokenModel
+    .create({
+      id: jwt.sign({exp: moment().unix() + parseInt(process.env.SESSION_EXPIRY, 10)}, process.env.SESSION_SECRET),
+      userId,
+    });
   }
   
 }
